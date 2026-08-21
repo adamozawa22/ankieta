3d14d36ac0a8ac7f958b4b991213abadfcd6cd5b
adamozawa22 <adamekkolanek46@gmail.com>
2026-08-21 22:03:58 +0000

    l

--- supabase-config.js
+++ supabase-config.js
@@ -5,4 +5,4 @@
 const SUPABASE_URL = 'https://spgtnyyqetlpewuvktqd.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNwZ3RueXlxZXRscGV3dXZrdHFkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODczNDQ3NTAsImV4cCI6MjEwMjkyMDc1MH0.B8eDOv4xZyfSMqjsWg0UFnkd2J8UzzpyJiZ4WECn0Io';
 
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const sb = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
