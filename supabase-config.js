9f54d540d33a8da5ca8691babb7f9008c61ecec0
adamozawa22 <adamekkolanek46@gmail.com>
2026-08-21 21:51:37 +0000

    lol

--- supabase-config.js
+++ supabase-config.js
@@ -2,7 +2,7 @@
 // Mozesz uzyc TEGO SAMEGO projektu co Genzie Hub - wystarczy,
 // ze wgralas/es supabase-schema.sql, ktory dodaje nowe tabele
 // (g4_codes, g4_questions, g4_answers) obok istniejacych.
-const SUPABASE_URL = 'https://TWOJ-PROJEKT.supabase.co';
-const SUPABASE_ANON_KEY = 'TWOJ-ANON-KEY';
+const SUPABASE_URL = 'sb_publishable_h5yUgjUn_VvwJLJBz6EsaA_eV3qZDUt';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNwZ3RueXlxZXRscGV3dXZrdHFkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODczNDQ3NTAsImV4cCI6MjEwMjkyMDc1MH0.B8eDOv4xZyfSMqjsWg0UFnkd2J8UzzpyJiZ4WECn0Io';
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
